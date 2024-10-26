cd68f2e2d77098aa48f1df5ec9b09a02229f3b12
李嘉图·M·路 <146103794+Ricardo-M-Zheng@users.noreply.github.com>
2024-10-26 11:43:42 +0800

    update

---
+++ src/components/side-drawer/side-drawer.tsx
@@ -0,0 +1,34 @@
+import { Component, Prop,h } from '@stencil/core';
+
+@Component({
+  tag: 'uc-side-drawer',
+  styleUrl: './side-drawer.css',
+  shadow: true
+})
+export class SideDrawer {
+  @Prop({ 
+    reflect: true }) title: string;
+  @Prop({ reflect: true, mutable: true }) open: boolean
+
+  onCloseDrawer() {
+    this.open = false;
+  }
+
+  render() {
+    return (
+      <aside>
+        <header>
+          <h1>{this.title}</h1>
+          <button onClick={this.onCloseDrawer.bind(this)}>X</button>
+        </header>
+        <section id="tabs">
+          <button class="active">NAVIGATION</button>
+          <button>CONTACT</button>
+        </section>
+        <main>
+          <slot />
+        </main>
+      </aside>
+    );
+  }
+}
